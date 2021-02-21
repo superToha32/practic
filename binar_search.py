26790f3d3504d95f7d04e77ea02140c11bafe750
ilya <superlokpop@mail.ru>
2021-02-21 17:36:04 +0300

    commit

---
+++ binar_search.py
@@ -0,0 +1,30 @@
+import time
+users = []
+
+for number in range(1000000):
+    new_user = {int(number),'user_' + str(number)}
+    users.append(new_user)
+
+
+
+
+inp = input()
+key = int(inp)
+array = range(len(users)+1)
+
+def binar_search(arr):
+   left = 0
+   right = arr[len(arr)-1]
+   while (left<=right):
+       mid = (right - left)//2 + left
+
+       if mid > key:
+           right = mid - 1
+       if mid < key:
+           left = mid + 1
+       if mid == key:
+           break###
+    ###
+   return mid###
+
+print(users[binar_search(array)])
